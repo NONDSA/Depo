a8848209f094294d91cbf1d46f1707913c4cd245
NONDSA <62667762+NONDSA@users.noreply.github.com>
2020-03-25 21:55:43 +0300

    Add files via upload

---
+++ ComLocker.bat
@@ -0,0 +1,160 @@
+@ECHO off & TITLE PROGRAM - KULLANICI:%username% &COLOR F0 &SET edc = exit &mode con: cols=30 lines=17
+IF EXIST "COSE.MICRO.LOXSERO.{00021401-0000-0000-C000-000000000046}"     GOTO LKXUEH73TX
+IF EXIST "Dosya\uuu\us\NPSWR.CZXNQ"                                      SET NSQ=1
+IF NOT EXIST "Dosya"                                                     GOTO IXYT366S7E 
+IF NOT EXIST "Dosya\uuu\us\PIF.mox"                                      GOTO SSYEG62TGZ
+IF NOT EXIST "Dosya\uuu"                                                 GOTO PSIEYXZF4D
+IF NOT EXIST "Dosya\uuu\us"                                              GOTO S7EGX5WT3E
+GOTO QX0MN6J
+    :QX0MN6J
+    IF NOT EXIST "Dosya\uuu\us\*.user"                                   GOTO KSUCU6DG6E
+    IF NOT EXIST "Dosya\uuu\us\*.ops"                                    GOTO KSUCU6DG6E
+    IF NOT EXIST "Dosya"                                                 GOTO IXYT366S7E
+    IF NOT EXIST "Dosya\uuu"                                             GOTO PSIEYXZF4D
+    IF NOT EXIST "Dosya\uuu\us"                                          GOTO S7EGX5WT3E
+    TITLE MENU - KULLANICI: %username%
+    CLS
+    ECHO ______________________________ &ECHO              MENU  &ECHO ______________________________ &ECHO. &ECHO 1- KiLiTLE &ECHO. &ECHO. &ECHO 2- CIKIS  &ECHO ______________________________
+        SET /p QX0MN6J=Numara :
+        IF %QX0MN6J%==1                                                  GOTO KDIRJ7WYYZ 
+        IF %QX0MN6J%==2                                                  GOTO KO46CYSRHH
+	    IF %QX0MN6J%==info                                               GOTO SKYEH92XY3
+        IF %QX0MN6J%==OS8CHW3JSAODK                 			         GOTO KSUCU6DG6E
+        IF %QX0MN6J%==NPQ023IDAJDZL                                      GOTO JJCUD7DGX6
+        IF %QX0MN6J%==PAKX29SJAP23I                                      GOTO IUXJE82YS2
+        IF %QX0MN6J%==KN32CXJ49SIAE                 			         GOTO KJS28XPN13
+        IF %QX0MN6J%==AIE93IDHA93J9                      		         GOTO KEQ8123URS
+        GOTO QX0MN6J
+            :OXUE8DUR
+            IF     EXIST NSQ==1 GOTO LKXUEH73TX
+            CLS &SET MWQ273FXCX=USCD\COREX\NPQ-*-*-*-*.RESCqSID &SET Q01=A
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=B
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=C
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=D
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=E
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=F
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=G
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=H
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=I
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=J
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=K
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=L
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=M
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=N
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=O
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=P
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=Q
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=R
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=S
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=T
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=U
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=V
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=W
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=X
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=Y
+            IF NOT EXIST %Q01%:\%MWQ273FXCX% SET Q01=Z
+            IF     EXIST %Q01%:\%MWQ273FXCX%                            GOTO  LKXUEH73TX
+            TITLE GEREKEN USB ALGILANAMADI &ECHO. &ECHO. &ECHO. &ECHO ______________________________ &ECHO         USB Yi TAKIN. &ECHO ______________________________ &PING LOCALHOS -N 3 >nul &GOTO OXUE8DUR
+                :LKXUEH73TX
+                TITLE giriŸ &CLS &ECHO ______________________________ &ECHO HESABINIZA GiRiS YAPIN. &ECHO ______________________________ &ECHO. &SET /p un=KULLANICI ADI : 
+                SET /p pw=KULLANICI PAROLASI : 
+                IF NOT EXIST "COSE.MICRO.LOXSERO.{00021401-0000-0000-C000-000000000046}\uuu\us\%un%.user" GOTO OXUE8DUR_NSZM92KJ
+                IF NOT EXIST "COSE.MICRO.LOXSERO.{00021401-0000-0000-C000-000000000046}\uuu\us\*-*-*-*-*-*-*-*-%pw%-*-*-%un%-*-*-*-*.ops" GOTO OXUE8DUR_NSZM92KJ
+                GOTO KN12HQJ
+                    :KN12HQJ
+                    ATTRIB -h -s "COSE.MICRO.LOXSERO.{00021401-0000-0000-C000-000000000046}" &REN "COSE.MICRO.LOXSERO.{00021401-0000-0000-C000-000000000046}" Dosya &ie4uinit -clearIconCache &GOTO KO46CYSRHH
+                        :OXUE8DUR_NSZM92KJ
+                        TITLE HATALI GiRiS &CLS &ECHO. &ECHO  Girdi§iniz KULLANICI ADI  &ECHO            veya  &ECHO        ŸIFre HATALI!  &PING LOCALHOS -N 3 >nul &GOTO OXUE8DUR
+                            :KO46CYSRHH
+                            exit
+:KDIRJ7WYYZ
+CLS &SET edc=%RANDOM% &ECHO kod: %edc% &SET /p msd=onay kodu:
+IF %msd%==%edc%                                                         GOTO NQW2J5BQ
+GOTO KDIRJ7WYYZ
+    :NQW2J5BQ
+    IF NOT EXIST "Dosya\uuu"                                            GOTO PSIEYXZF4D
+    IF NOT EXIST "Dosya\uuu\us"                                         GOTO ID62YX66
+    REN Dosya "COSE.MICRO.LOXSERO.{00021401-0000-0000-C000-000000000046}"  &ATTRIB +h +s "COSE.MICRO.LOXSERO.{00021401-0000-0000-C000-000000000046}" &ie4uinit -clearIconCache &GOTO KO46CYSRHH
+        :KJS28XPN13
+        CLS &ECHO. &ECHO ______________________________ &ECHO Parolalari silmek icin dogrula &ECHO ______________________________ &ECHO. &ECHO. &SET edc=%RANDOM% &ECHO kod: %edc% &SET /p msd=onay kodu:
+        IF %msd%==%edc%                                                 GOTO XJEUU93P
+        GOTO KJS28XPN13
+            :XJEUU93P
+            RD /S /Q "Dosya\uuu" &GOTO PSIEYXZF4D
+                :IXYT366S7E 
+                CLS &md Dosya &GOTO PSIEYXZF4D
+                    :PSIEYXZF4D
+                    CLS &md "Dosya\uuu" &ATTRIB +h +s "Dosya\uuu"
+                        :S7EGX5WT3E
+                        CLS &md "Dosya\uuu\us" &ATTRIB +h +s "Dosya\uuu\us" &ECHO 000-000-000-000-%date%-000-000-%time%-000-000>Dosya\uuu\us\PIF.mox
+                            :TXERX239
+                            SET /P USBNEX=USB KORUMA (A/K):
+                            IF %USBNEX%==A GOTO PXCQB7RD
+                            IF %USBNEX%==a GOTO PXCQB7RD
+                            IF %USBNEX%==K GOTO KSUCU6DG6E
+                            IF %USBNEX%==k GOTO KSUCU6DG6E
+                            GOTO TXERX239
+                            :PXCQB7RD
+                            SET MNC6DO4IS=%CD% &CLS &SET /p nxs=KAYIT edilecek USB etiketi:
+                            IF     EXIST %nxs%:\ GOTO ORS84HD5SF
+                            IF NOT EXIST %nxs%:\ ECHO Bu etiket gecersiz. &PING LOCALHOS -N 2 >nul &GOTO TXERX239
+                            :ORS84HD5SF
+                            IF NOT EXIST %nxs%:\USCD         md %nxs%:\USCD &ATTRIB +h +s %nxs%:\USCD
+                            IF NOT EXIST %nxs%:\USCD\COREX   md %nxs%:\USCD\COREX
+                            IF     EXIST %nxs%:\USCD\COREX   ATTRIB +h +s %nxs%:\USCD\COREX 
+                            IF     EXIST %nxs%:\USCD\COREX   ATTRIB +h +s %nxs%:\USCD\COREX\*.*
+                            ECHO ID: %RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM% > %nxs%:\USCD\COREX\NPQ-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.RESCqSID &GOTO KSUCU6DG6E
+:create
+IF NOT EXIST "Dosya\uuu"                                               GOTO PSIEYXZF4D
+IF NOT EXIST "Dosya\uuu\us"                                            GOTO S7EGX5WT3E
+TITLE HESAP OLUSTUR &GOTO KSUCU6DG6E
+    :KSUCU6DG6E
+    CD %MNC6DO4IS%
+    IF NOT EXIST "Dosya"                                                   GOTO IXYT366S7E
+    IF NOT EXIST "Dosya\uuu"                                               GOTO PSIEYXZF4D
+    IF NOT EXIST "Dosya\uuu\us"                                            GOTO S7EGX5WT3E 
+    CLS &ECHO ______________________________ &ECHO          Hesap oluŸtur  &ECHO ______________________________ &ECHO. &SET /p nun=Yein KULLANICI ADI : 
+    SET /p npw=Yeni KULLANICI PAROLASI : 
+    IF EXIST "Dosya\uuu\us\%nun%.user"                                 GOTO create_NSZM92KJ
+    ECHO ID: %RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM% >"Dosya\uuu\us\%nun%.user" &ECHO ID: %RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM% >"Dosya\uuu\us\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%npw%-%RANDOM%-%RANDOM%-%nun%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.ops" &ATTRIB +h +s "Dosya\uuu\us\" &CLS &ECHO. &ECHO. &ECHO. &ECHO. &ECHO. &ECHO. &ECHO ______________________________ &ECHO Hesap baŸaryla oluŸturuldu.! &ECHO ______________________________ &PING LOCALHOS -N 3 >nul &GOTO QX0MN6J
+        :create_NSZM92KJ
+        CLS &TITLE HATALI kayıt &ECHO. &ECHO Bu KULLANICI ADI ZATEN mevcut. &ECHO LUTFEN TEKRAR DENEYiN... &PING LOCALHOS -N 3 >nul & GOTO KSUCU6DG6E
+            :SSYEG62TGZ
+            CLS &ECHO. &ECHO. &ECHO. &ECHO. &ECHO      Burada bizim systemin  &ECHO      olusturmadigi "Dosya"  &ECHO          adinda klador  &ECHO       yada bir dosya var. &ECHO        system Dosyanin &ECHO        adini Dosya.old &ECHO       olarak degistirecek. &ECHO. &ECHO      devam etmek istersen  &ECHO     herhangi bir harfe bas &pause>nul &REN Dosya Dosya.old &ie4uinit -clearIconCache &GOTO IXYT366S7E
+                :IUXJE82YS2
+                CLS
+                IF NOT EXIST Dosya GOTO Looper
+                ATTRIB -h -s Dosya\uuu\us &pause>nul &ATTRIB -h -s Dosya\uuu &pause>nul &ATTRIB +h +s Dosya\uuu\us &ATTRIB +h +s Dosya\uuu &GOTO KO46CYSRHH
+                    :Looper
+                    ECHO burada istenen dosya yok ! &pause>nul &GOTO KO46CYSRHH
+                        :SKYEH92XY3
+                        CLS &ECHO ______________________________  &ECHO. &ECHO kod sahibi : T**** Y**** &ECHO. &ECHO olusturulma amaci :  &ECHO Kisisel verilere erisimi &ECHO kisitlamak ya da engellemek. &ECHO kaynak :  &ECHO google.com, www.dostips.com &ECHO youtube.com, hamdicatal.com, &ECHO www.chip.com.tr,  &ECHO ve www.solvusoft.com &ECHO. &ECHO Bu kod ticari amaclarla  &ECHO    tasarlanmamistir. &ECHO. &ECHO ______________________________  &pause>nul &GOTO QX0MN6J
+:KEQ8123URS
+TITLE GiRiS &CLS &ECHO ______________________________ &ECHO HESABINIZA giriŸ yapn. &ECHO ______________________________ &ECHO. &SET /p un=KULLANICI ADIin : 
+SET /p pw=KULLANICI Parolan : 
+IF NOT EXIST "Dosya\uuu\us\%un%.user"                                   GOTO MNDY7WYYXF
+IF NOT EXIST "Dosya\uuu\us\*-*-*-*-*-*-*-*-%pw%-*-*-%un%-*-*-*-*.ops"   GOTO MNDY7WYYXF
+GOTO KSURHC63H
+    :KSURHC63H
+    CLS &ECHO ______________________________ &ECHO          Hesap oluŸtur  &ECHO ______________________________ &ECHO. &SET /p nun=Yein KULLANICI ADI : 
+    SET /p npw=Yeni KULLANICI Parolas : 
+    IF EXIST Dosya\uuu\us\%nun%.user                                  GOTO XKEXU377DY
+    del Dosya\uuu\us\%un%.user &cls &del Dosya\uuu\us\*-*-*-*-*-*-*-*-%pw%-*-*-%un%-*-*-*-*.ops &cls &RD /S /Q Dosya\uuu\us\%un%.user &cls %RD /S /Q Dosya\uuu\us\*-*-*-*-*-*-*-*-%pw%-*-*-%un%-*-*-*-*.ops &cls &ECHO ID: %RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM% >Dosya\uuu\us\%nun%.user &ECHO ID: %RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM% >"Dosya\uuu\us\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%-%npw%-%RANDOM%-%RANDOM%-%nun%-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.ops" &CLS &ECHO. &ECHO. &ECHO. &ECHO. &ECHO. &ECHO. &ECHO ______________________________ &ECHO Hesap baŸaryla Degistirildi! &ECHO ______________________________ &PING LOCALHOS -N 3 >nul &GOTO QX0MN6J
+        :MNDY7WYYXF
+        TITLE HATALI GiRiS &CLS &ECHO. &ECHO  GiRDiGiN KULLANICI ADI  &ECHO            veya  &ECHO        SiFRE HATALI! &PING LOCALHOS -N 3 >nul &GOTO KEQ8123URS
+            :XKEXU377DY
+            CLS &TITLE HATALI kayıt &ECHO. &ECHO BU KULLANICI ADI ZATEN MEVCUT. &ECHO LUTFEN TEKRAR DENEYiN... &PING LOCALHOS -N 3 >nul &GOTO KSURHC63H
+                :JJCUD7DGX6
+                TITLE USB KORUMA KALDIRMA iSTEMCiSİ &CLS &SET edc=%RANDOM% &ECHO kod: %edc% &ECHO iPTAL iCiN 0 TUSA BAS. &SET /p msd=KALDIRMA ONAYI:
+                IF %msd%==0 GOTO QX0MN6J
+                IF %msd%==%edc% SET USHE37UD=K
+                CLS &SET /p nxs=KALDIRMAK iCin KAYITLI USB ETiKETi:
+                IF NOT EXIST %nxs%:\USCD ECHO Bu USB'de koruma KAYDI YOK. &PING LOCALHOS -N 2 >nul &GOTO JJCUD7DGX6
+                IF NOT EXIST %nxs%:\ ECHO Bu etiket gecersiz. &PING LOCALHOS -N 2 >nul &GOTO JJCUD7DGX6
+                IF %USHE37UD%==K RD /S /Q %nxs%:\USCD
+                GOTO QX0MN6J
+                
+::
+:: CODE: ANSI
+:: This Code writed by Cropration@Qalel
+:: 
